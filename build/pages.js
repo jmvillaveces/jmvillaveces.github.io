9ba194f15524652889ab26ece6bceadee1a9e8f5
jVillaveces <secevalliv@gmail.com>
2015-02-10 18:04:56 +0100

    now printing two pages

--- build/pages.js
+++ build/pages.js
@@ -3,13 +3,13 @@ jQuery = $ = require('jquery');
 require('bootstrap');
 
 $('#pdf').click(function(e){
-
-    $('#container').css('background-color', 'white');
-     
+    
     var pdf = new jsPDF('p','pt','a4');
-    pdf.addPage();
-    pdf.addHTML($('#container').get(0),function() {
-        pdf.save('JMV_cv.pdf');
+    pdf.addHTML($('#page1').get(0),function() {
+        pdf.addPage();
+        pdf.addHTML($('#page2').get(0),function() {
+            pdf.save('JMV_cv.pdf');
+        });
     });
     
 });
--- index.js
+++ index.js
@@ -2,12 +2,13 @@ jQuery = $ = require('jquery');
 require('bootstrap');
 
 $('#pdf').click(function(e){
-
-    $('#container').css('background-color', 'white');
-     
+    
     var pdf = new jsPDF('p','pt','a4');
-    pdf.addHTML($('#container').get(0),function() {
-        pdf.save('JMV_cv.pdf');
+    pdf.addHTML($('#page1').get(0),function() {
+        pdf.addPage();
+        pdf.addHTML($('#page2').get(0),function() {
+            pdf.save('JMV_cv.pdf');
+        });
     });
     
 });
