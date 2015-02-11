a6cc1f57dfa5bbaf656086c0ecb2359e1c155570
jVillaveces <secevalliv@gmail.com>
2015-02-11 16:47:33 +0100

    hiding data

--- build/pages.js
+++ build/pages.js
@@ -2,18 +2,31 @@
 jQuery = $ = require('jquery');
 require('bootstrap');
 
+$('#hiddata').hide();
 $('#pdf').click(function(e){
     
+    $('#hiddata').show();
     var pdf = new jsPDF('p','pt','a4');
-    pdf.addHTML($('#page1').get(0),function() {
+    pdf.addHTML($('#page1').get(0), function() {
+        $('#hiddata').hide();
         pdf.addPage();
-        pdf.addHTML($('#page2').get(0),function() {
+        pdf.addHTML($('#page2').get(0), function() {
             pdf.save('JMV_cv.pdf');
         });
     });
     
 });
 
+$('#logo').mouseover(function(){
+    $('#logo').attr('src', 'img/space_invader2.png');
+});
+
+
+$('#logo').mouseout(function(){
+    setTimeout(function(){
+        $('#logo').attr('src', 'img/space_invader.png');
+    }, 800);
+});
 
 
 },{"bootstrap":2,"jquery":15}],2:[function(require,module,exports){
